7ae6aeca2a0cdd12c38c0e662f9f5f52a95c0438
João <joaovitoremailprofissional@gmail.com>
2024-10-22 16:03:44 -0300

    update

--- projeto2/main.cpp
+++ projeto2/main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
-
+#include <QWidget>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Exemplo de Mudança de Plano de Fundo");
+    w.resize(400, 300);
+
+    // Cria um novo objeto QPalette
+    QPalette palette;
+    // Define a cor de fundo para o widget
+    palette.setColor(QPalette::Window, Qt::blue);
+
+    // Aplica o novo QPalette ao widget
+    w.setPalette(palette);
     w.show();
 
     return a.exec();
